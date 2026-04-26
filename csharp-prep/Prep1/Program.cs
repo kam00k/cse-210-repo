289665a271ad263d9147b94adb25f71f9f2c2240
kam00k <jawilson97@gmail.com>
2026-04-25 18:00:54 -0600

    changed readme

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,19 @@
+//Jack Wilson
+//CSE210
+//C# Prep 1
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+
+        Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,50 @@
+//Jack Wilson
+//CSE210
+//C# Prep 2
+
 using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        int grade = Convert.ToInt32(Console.ReadLine());
+
+        char letterGrade;
+
+        if (grade < 60)
+        {
+            letterGrade = 'F';
+        }
+        else if (grade < 70)
+        {
+            letterGrade = 'D';
+        }
+        else if (grade < 80)
+        {
+            letterGrade = 'C';
+        }
+        else if (grade < 90)
+        {
+            letterGrade = 'B';
+        }
+        else
+        {
+            letterGrade = 'A';
+        }
+
+
+        Console.WriteLine($"Your grade for this course is {letterGrade}.");
+        if (grade > 70)
+        {
+            Console.WriteLine("You passed! Congratulations");
+        }
+        else
+        {
+            Console.WriteLine("You did not achieve a passing grade. Try again next time.");
+        }
     }
 }
