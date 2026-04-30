05a9f96a0e690a1d0d0f98430cc3f3d66882c3df
kam00k <jawilson97@gmail.com>
2026-04-29 21:27:39 -0600

    Completed Abstraction Learning Project

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,43 @@
+//Jack Wilson
+//CSE210
+//Abstraction Learning Assignment
+
 using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Harmony Energy Works";
+        job1._jobTitle = "Grant Application Assistant";
+        job1._startYear = 2015;
+        job1._endYear = 2018;
+
+        //job1.Display();
+
+        Job job2 = new Job();
+        job2._company = "Harmony Energy Investments";
+        job2._jobTitle = "Group Net Metering Specialist";
+        job2._startYear = 2020;
+        job2._endYear = 2026;
+
+        //job2.Display();
+
+        Resume resume1 = new Resume();
+        resume1._name = "Jack Wilson";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
+
+
     }
+
+
+
+    
+
+
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,18 @@
+//Jack Wilson
+//CSE210
+//Abstraction Learning Assignment
+
+public class Resume
+    {
+        public string _name;
+        public List<Job> _jobs = new List<Job>();
+
+        public void Display()
+        {
+            Console.WriteLine($"Name: {_name}");
+            Console.WriteLine("Jobs:");
+            foreach(var job in _jobs){
+                job.Display();
+            }
+        }
+    }
