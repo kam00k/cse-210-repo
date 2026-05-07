f13c2b7730b0ef40c29ca93ea750e02f55d8e4eb
kam00k <jawilson97@gmail.com>
2026-05-07 10:01:48 -0600

    Initial Commit on Week 3 Develop Project

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,32 @@
+class Scripture
+{
+    private string _scriptureText;
+    private string _scriptureWordList;
+    private Reference _scriptureReference;
+
+    public Scripture(string scriptureText)
+    {
+        
+    }
+
+    public Scripture(string scriptureText, string reference)
+    {
+        
+    }
+
+    public string GetRenderedText()
+    {
+        string text;
+        return text;
+    }
+
+    public void HideStep()
+    {
+        
+    }
+
+    public bool IsAllHidden()
+    {
+        
+    }
+}
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,22 @@
+class Word
+{
+    private string _word;
+    private bool _isHidden;
+
+    public Word(string word)
+    {
+        _word = word;
+        _isHidden = false;
+    }
+
+    public void ConvertToUnderscore()
+    {
+        
+    }
+
+    public string GetWordString()
+    {
+        string wordString = _word;
+        return wordString;
+    }
+}
