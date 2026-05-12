14fc980ff0570f9cb2893fb3696b25cbb763adf5
kam00k <jawilson97@gmail.com>
2026-05-12 12:41:37 -0600

    Completed W04 Learning Assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Rocky Balboa", "Culinary Arts");
+        Console.WriteLine(assignment.GetSummary());
+        MathAssignment math = new MathAssignment("Brad Williams", "Control systems", "3.6", "8-9");
+        Console.WriteLine(math.GetSummary() + "\n" + math.GetHomeworkList());
+        WritingAssignment write = new WritingAssignment("Tsukiyuki Miyako", "Cuniculture", "28 Baby Rabbits");
+        Console.WriteLine(write.GetSummary() + "\n" + write.GetWritingInformation());
     }
+
 }
