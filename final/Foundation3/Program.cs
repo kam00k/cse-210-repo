5940a26b264603dd602d0c941ec709065b5edbbc
kam00k <jawilson97@gmail.com>
2026-06-02 14:46:37 -0600

    Minor formatting changes on Program 3

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Lecture lecture = new Lecture("Lagrangian points", "Come hear a fascinating speech on the secret regions of space where gravity itself cancels out.", "September 15th, 2008", "7:00PM", "Dr. Andrew Lariviere", 125);
+        Lecture lecture = new Lecture("Lagrangian Points: Mysterious Gravity Bowls", "Come hear a fascinating speech on the secret regions of space where gravity itself cancels out.", "September 15th, 2008", "7:00PM", "Dr. Andrew Lariviere", 125);
         Reception reception = new Reception("Billy Kid and Nicole's Wedding", "We never thought we'd see the day but our Billy's all grown up and found himself a sweetheart! Come celebrate with everyone, the more the merrier!", "02/23/2024", "4:30PM", "bobbykidd1962@hotmail.com");
         Outdoor outdoor = new Outdoor("Guy Fawkes' Night", "Ever wanted to burn an effigy of the OG political terrorist? Well, come on down and have some outside fun. There'll be a massive bonfire too!", "November 5th, 2018", "16:30", "It's expected to be a clear night, but will be chilly if you're not near the fire. Dress however you like, but be prepared for the cold!");
 
@@ -17,7 +17,6 @@ class Program
         lecture.ShortDescription();
 
         reception.StandardDescription();
-        Console.WriteLine("");
         reception.FullDescription();
         reception.ShortDescription();
     
