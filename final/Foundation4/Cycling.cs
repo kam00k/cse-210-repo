8bfbee77882cd6837c7834e10c48fc45affed865
kam00k <jawilson97@gmail.com>
2026-06-02 18:04:32 -0600

    Removed unnecessary "using" statements in Program 4

--- final/Foundation4/Cycling.cs
+++ final/Foundation4/Cycling.cs
@@ -1,4 +1,4 @@
-using System.ComponentModel.DataAnnotations;
+
 
 public class Cycling : Activity
 {
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Diagnostics;
-using Microsoft.VisualBasic;
 
 class Program
 {
